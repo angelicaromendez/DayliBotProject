de8a7f5b4dde2c98387a1b928b35baba6e4142ab
angelicama71 <166411855+angelicama71@users.noreply.github.com>
2024-11-04 22:05:23 -0600

    Se agrego a Views
    Se agrego la clase read and update.

--- Bot/views.py
+++ Bot/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from models import Bot
 from serializers import BotSerializer
 
@@ -14,3 +18,24 @@ class CreateBotView(APIView):
         serializer = BotSerializer(data=data)
         serializer.is_valid(raise_exception=True)   
         serializer.save()
+
+        return Response ({'message': 'Bot created successfully'}, status= status.HTTP_201_CREATED)
+
+class RetrieveBotView(APIView):
+    
+    def get(self, request, bot_id):
+        bots_obj = get_object_or_404(Bot, id=bot_id)
+        serializer = BotSerializer(bots_obj)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateBotView(APIView):
+    
+    def put(self, request, bot_id):
+        data = request.data
+        bots_obj = get_object_or_404(Bot, id=bot_id)
+        serializer = BotSerializer(instance=bots_obj, data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'message': 'Bot updated successfully'}, status=status.HTTP_200_OK)
