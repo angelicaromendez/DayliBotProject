5ec499b422442d6da247539a68e89a77bc580961
angelicama71 <166411855+angelicama71@users.noreply.github.com>
2024-11-19 18:03:57 -0600

    tratando con el error
    el error perciste

--- Bot/views.py
+++ Bot/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from models import Bot
-from serializers import BotSerializer
+from Bot.serializers import BotSerializer
 
 
 # Create your views here.
