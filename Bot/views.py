6deb6e1bff1dac5c9600ac04e31a22970d22da57
Kerly <kerlyeliz19@hotmail.com>
2024-11-21 14:11:36 -0500

    fixed  model

--- Bot/views.py
+++ Bot/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Bot
+# from .models import Bot
 from Bot.serializers import BotSerializer
 
 
