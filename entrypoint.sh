cad54a6737592a01d9588fc45f966ef2957b919a
Kerly <kerlyeliz19@hotmail.com>
2024-11-22 01:46:46 -0500

    version final 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-set -e  # Termina el script si ocurre un error
+set -e
 
-echo "Aplicando migraciones..."
+echo "Checking DATABASE_URL..."
+if [ -z "$DATABASE_URL" ]; then
+  echo "Error: DATABASE_URL is not set."
+  exit 1
+else
+  echo "DATABASE_URL is set."
+fi
+
+echo "Applying migrations..."
 python manage.py migrate
 
-echo "Creando superusuario..."
+echo "Creating superuser..."
 python manage.py createsuperuser --noinput \
     --username "$DJANGO_SUPERUSER_USERNAME" \
     --email "$DJANGO_SUPERUSER_EMAIL" || true
 
-echo "Iniciando Gunicorn..."
+echo "Starting Gunicorn..."
 gunicorn --bind 0.0.0.0:"$PORT" DayliBot.wsgi:application
